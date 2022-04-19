c3db7e8cfe607610b5c0eb4efcf164a0e016db00
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-04-19 14:42:31 -0400

    mini TS help

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -2,13 +2,29 @@ import type { GetServerSideProps, NextPage } from "next";
 import { Layout } from "../../components/layout";
 import { Recipe } from "../../types";
 
-const Recipe: NextPage<Recipe> = (props) => {
+type Props = {
+  recipe: Recipe;
+};
+
+const Recipe: NextPage<Props> = ({ recipe }) => {
   return <Layout>{}</Layout>;
 };
 
 export default Recipe;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // keep this here to ensure typescript is happy
+  if (typeof context.params?.id !== "string") {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  // data fetching will go here
+
   return {
     props: {},
   };
