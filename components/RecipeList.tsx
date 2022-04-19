f5fa16f20f3aa9881cf52021feb5d4d934dd2279
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-04-19 14:09:15 -0400

    recipe component rework

--- components/RecipeList.tsx
+++ components/RecipeList.tsx
@@ -12,10 +12,10 @@ export const RecipeList: React.VFC<Props> = ({ recipes }) => {
           <li key={recipe.id} className="py-5">
             <div className="relative focus-within:ring-2 focus-within:ring-indigo-500">
               <h3 className="text-sm font-semibold text-gray-800">
-                <a href="#" className="hover:underline focus:outline-none">
+                <p className="hover:underline focus:outline-none">
                   <span className="absolute inset-0" aria-hidden="true" />
                   {recipe.name}
-                </a>
+                </p>
               </h3>
               <p className="mt-1 text-sm text-gray-600 line-clamp-2">
                 {recipe.ingredients.join(", ")}
