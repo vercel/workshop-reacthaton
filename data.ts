e585d6e443550dd7f6db398f37be8b792283fa07
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2022-04-22 14:24:54 -0400

    recipes in json

---
+++ data.ts
@@ -0,0 +1,70 @@
+export const recipes = [
+  {
+    id: 3,
+    created_at: "2022-04-13T19:17:56+00:00",
+    ingredients: [
+      "1 oz. (30ml) Grenadine",
+      "1/2 oz. (15ml) Peach Schnapps",
+      "1 1/2 oz. (45ml) Pineapple Juice",
+      "1 oz. (30ml) Vodka",
+      "1/2 oz. (15ml) Blue Curaçao",
+    ],
+    likes: 0,
+    name: "Taste the Rainbow",
+    steps: [
+      "Pour some Grenadine into the base of a glass and slowly spoon in some ice cubes. Set aside.",
+      "In a shaking glass with ice, combine peach schnapps and pineapple juice. Shake well.",
+      "Slowly pour over grenadine, filling almost to the top.",
+    ],
+  },
+  {
+    id: 4,
+    created_at: "2022-04-13T19:22:35+00:00",
+    ingredients: [
+      "6 oz. (180ml) Champagne",
+      "1/2 oz. (15ml) Melon Liqueur",
+      "Splash Blue Curacao",
+      "2 oz. (60ml) Pineapple Juice",
+      "Garnish: Blue Sugar/Pineapple Juice",
+    ],
+    likes: 0,
+    name: " MERMAID MIMOSA",
+    steps: [
+      "Rim edge of champagne flute with blue sugar. Add melon liqueur, blue curacao and pineapple juice.",
+      "Top with champagne and garnish with pineapple wedge DRINK RESPONSIBLY!",
+    ],
+  },
+  {
+    id: 5,
+    created_at: "2022-04-13T19:23:55+00:00",
+    ingredients: [
+      "1 oz. (30ml) Raspberry Vodka",
+      "1 oz. (30ml) Kinky Pink Liqueur",
+      "3 oz. (90ml) Strawberry Lemonade",
+      "Lemon Lime Soda",
+      " Rim: Pink Sugar ",
+      "Garnish: Lime Slice ",
+    ],
+    likes: 0,
+    name: "The Pink Man",
+    steps: [
+      "Rim the edge of your glass using pink sugar. Set aside. ",
+      "In a shaking glass with ice, combine raspberry vodka, kinky pink liqueur, and strawberry lemonade. Shake well.",
+    ],
+  },
+  {
+    id: 2,
+    created_at: "2022-04-13T19:16:29+00:00",
+    ingredients: [
+      "1 1/2 oz of Orange Juice",
+      "4 oz of 7-up soda (or Sprite)",
+      "1 oz of Vodka",
+      "3 dash of cherry juice",
+    ],
+    likes: 2,
+    name: "Drag Queen Cocktail",
+    steps: [
+      "Put ice in the glass, then add the vodka first, then the 7-up, then the orange juice, and then the cherry juice. Top with a cherry and an orange slice or leave it plain. ",
+    ],
+  },
+];
